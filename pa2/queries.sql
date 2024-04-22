f33d2f81decf90de68b33a5fcab0f5f70f984c86
vsahaidak06 <vsahaidak@kse.org.ua>
2024-04-22 10:23:07 +0300

    pa2 done

---
+++ pa2/queries.sql
@@ -0,0 +1,21 @@
+﻿-- books, which have more than one genre
+
+SELECT b.id, b.title, COUNT(gb.genre_id) AS amount_genres
+FROM book b
+         INNER JOIN genre2book gb ON b.id = gb.book_id
+GROUP BY b.id, b.title
+HAVING COUNT(gb.genre_id) > 1
+ORDER BY amount_genres DESC, b.title;
+
+
+-- authors, who works together the most times
+
+SELECT b.id, b.author_id,
+       GROUP_CONCAT(a.name ORDER BY a.name SEPARATOR ', ') AS author_name,
+       b.title AS book_title
+FROM author a
+         INNER JOIN author2book ab ON a.id = ab.author_id
+         INNER JOIN book b ON b.id = ab.book_id
+GROUP BY b.id
+HAVING COUNT(b.author_id) > 1
+ORDER BY author_name;
---
+++ pa2/schema_updates.sql
@@ -0,0 +1,97 @@
+﻿CREATE TABLE author2book (
+     author_id INTEGER,
+     book_id INTEGER,
+     PRIMARY KEY (author_id, book_id),
+     FOREIGN KEY (author_id) REFERENCES author(id),
+     FOREIGN KEY (book_id) REFERENCES book(id)
+);
+
+INSERT INTO author2book (author_id, book_id) VALUES
+     (1, 18),
+     (2, 17),
+     (3, 16),
+     (10, 15),
+     (4, 14),
+     (5, 14),
+     (6, 13),
+     (7, 12),
+     (7, 11),
+     (8, 10),
+     (9, 9),
+     (10, 8),
+     (11, 8),
+     (12, 7),
+     (12, 6),
+     (13, 5),
+     (11, 15),
+     (15, 4),
+     (16, 3),
+     (14, 2),
+     (17, 1),
+     (18, 1);
+
+
+CREATE TABLE genre2book (
+    genre_id INTEGER,
+    book_id INTEGER,
+    PRIMARY KEY (genre_id, book_id),
+    FOREIGN KEY (genre_id) REFERENCES genre(id),
+    FOREIGN KEY (book_id) REFERENCES book(id)
+);
+
+INSERT INTO genre2book (genre_id, book_id) VALUES
+       (1, 18),
+       (2, 17),
+       (3, 16),
+       (3, 15),
+       (4, 14),
+       (5, 8),
+       (6, 13),
+       (7, 12),
+       (2, 11),
+       (2, 10),
+       (3, 9),
+       (3, 8),
+       (4, 8),
+       (2, 7),
+       (5, 6),
+       (5, 5),
+       (4, 4),
+       (1, 4),
+       (1, 3),
+       (2, 2),
+       (7, 1),
+       (6, 1);
+
+
+CREATE TABLE book2loan (
+       book_id INTEGER,
+       loan_id INTEGER,
+       PRIMARY KEY (book_id, loan_id),
+       FOREIGN KEY (book_id) REFERENCES book(id),
+       FOREIGN KEY (loan_id) REFERENCES loan(id)
+);
+
+INSERT INTO book2loan (book_id, loan_id) VALUES
+     (1, 17),
+     (2, 17),
+     (3, 16),
+     (3, 15),
+     (4, 14),
+     (5, 14),
+     (6, 13),
+     (7, 12),
+     (9, 11),
+     (8, 10),
+     (9, 9),
+     (10, 8),
+     (11, 8),
+     (12, 7),
+     (12, 6),
+     (13, 5),
+     (14, 4),
+     (15, 4),
+     (15, 3),
+     (16, 2),
+     (17, 1),
+     (18, 6);
